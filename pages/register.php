7a20800d2f647c12b46d7195c97f008a5f702751
dratomitoma <tomasxavier03@gmail.com>
2023-04-21 15:14:25 +0100

    combined css

--- pages/register.php
+++ pages/register.php
@@ -5,30 +5,31 @@
     include('../templates/header.php');
 
 ?>
-
+<body id=register_body>
     <main id= "register_page">
         <section id="login_box">
-            <form action="login.php" method="post">
+            <form action="login.php" method="post" id="register_form">
                 <header>
                     <h3>Register</h3>
                 </header>
-                <div class="inputbox">
+                <div class="inputbox" id="register_inputbox">
                     <input type="text" name="username" placeholder="E-mail" required>
                 </div>
-                <div class="inputbox">
+                <div class="inputbox" id="register_inputbox">
                     <input type="text" name="username" placeholder="Username" required>
                 </div>
-                <div class="inputbox">
+                <div class="inputbox" id="register_inputbox">
                     <input type="password" name="password" placeholder="Password" required>
                 </div>
-                <div class="inputbox">
+                <div class="inputbox" id="register_inputbox">
                     <input type="password" name="password" placeholder="Confirm Password" required>
                 </div>
-                <button>Register</button>
-                <p>Already have an account? <a href="login.html">Login Here</a></p>
+                <button id="register_button">Register</button>
+                <p>Already have an account? <a href="login.php">Login Here</a></p>
             </form>
         </section>
     </main>
+</body>
 <?php
 
     include('../templates/footer.php');
