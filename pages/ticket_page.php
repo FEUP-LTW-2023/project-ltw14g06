9251f9f4581d817237f94abf7b6aee8e69c09508
Tonevanda <up202108863@up.pt>
2023-05-21 17:16:28 +0100

    more css

--- pages/ticket_page.php
+++ pages/ticket_page.php
@@ -29,58 +29,50 @@
     <?php if($_SESSION["type"] !== 'Client'){?>
         <section id="ticket_page_section">
 
-            <a href="../pages/ticket_changelog.php?id=<?php echo $ticketId; ?>" id = ticket_changelog>Ticket History</a>
-    
-            <form action="../actions/change_ticket_department_action.php" class="editTicketForm" method="post">
-                <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
-                <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
-                <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-                <input type="hidden" value = "<?php echo getDepartmentName($ticket['department_id']) ?>" name = "old_department">
-                <div class="changeTicket_inputBox">
-                    <p>Change Department: 
-                        <select name="ticket_department" id="ticket_department">
-                            <?php
-                                foreach ($departments as $department) {
-                                    if($department["id"] === $ticket["department_id"]) continue;
-                                    echo '<option value="' . $department["id"] . '">' . $department["name"] . '</option>';
-                                }
-                            ?>
-                        </select>   
-                    </p>
-                </div>
-                <button type="submit">Submit Change Department</button>
-            </form>
-    
-            <form action="../actions/change_ticket_priority_action.php" class="editTicketForm" method="post">
-                <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
-                <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
-                <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-                <input type="hidden" value = "<?php echo $ticket['priority'] ?>" name = "old_priority">
-                <div class="changeTicket_inputBox">
-                    <p>Change Priority: 
-                        <select name="ticket_priority" id="ticket_priority">
-                            <?php if($ticket["priority"]!=="Low"){?>
-                                <option value="Low">Low</option>
-                            <?php } if($ticket["priority"]!=="Medium"){?>
-                                <option value="Medum">Medium</option>
-                            <?php }if($ticket["priority"]!=="High"){ ?>
-                                <option value="High">High</option>
-                            <?php } ?>
-                        </select>   
-                    </p>
-                </div>
-                <button type="submit">Submit Change Priority</button>
-            </form>
-    
-            <form action="../actions/change_ticket_status_action.php" class="editTicketForm" method="post">
-                <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
-                <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
-                <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-                <input type="hidden" value = "<?php echo $ticket['agent_id'] ?>" name = "old_agent">
-                <input type="hidden" value = "<?php echo getStatusName($ticket['status_id']) ?>" name = "old_status">
-                <div class="changeTicket_inputBox">
-                    <p>Change Status: 
-                    <select name="ticket_status" id="ticket_status">
+            <a href="../pages/ticket_changelog.php?id=<?php echo $ticketId; ?>" id = "ticket_changelog">Ticket History</a>
+            
+            <section id="edit_ticket_section">
+
+                <form action="../actions/change_ticket_department_action.php" class="editTicketForm" method="post">
+                    <input id="csrf"type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
+                    <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
+                    <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                    <input type="hidden" value = "<?php echo getDepartmentName($ticket['department_id']) ?>" name = "old_department">
+                    <select name="ticket_department" id="change_ticket_department" class="editTicketInputbox">
+                        <?php
+                            foreach ($departments as $department) {
+                                if($department["id"] === $ticket["department_id"]) continue;
+                                echo '<option value="' . $department["id"] . '">' . $department["name"] . '</option>';
+                            }
+                        ?>
+                    </select>   
+                    <button class = "changeTicketInfoButton" id="change_ticket_department_button" type="submit">Submit Change Department</button>
+                </form>
+        
+                <form action="../actions/change_ticket_priority_action.php" class="editTicketForm" method="post">
+                    <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
+                    <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
+                    <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                    <input type="hidden" value = "<?php echo $ticket['priority'] ?>" name = "old_priority">
+                    <select name="ticket_priority" id="change_ticket_priority" class="editTicketInputbox">
+                        <?php if($ticket["priority"]!=="Low"){?>
+                            <option value="Low">Low</option>
+                        <?php } if($ticket["priority"]!=="Medium"){?>
+                            <option value="Medum">Medium</option>
+                        <?php }if($ticket["priority"]!=="High"){ ?>
+                            <option value="High">High</option>
+                        <?php } ?>
+                    </select>   
+                    <button class = "changeTicketInfoButton" id="change_ticket_priority_button" type="submit">Submit Change Priority</button>
+                </form>
+        
+                <form action="../actions/change_ticket_status_action.php" class="editTicketForm" method="post">
+                    <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
+                    <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
+                    <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                    <input type="hidden" value = "<?php echo $ticket['agent_id'] ?>" name = "old_agent">
+                    <input type="hidden" value = "<?php echo getStatusName($ticket['status_id']) ?>" name = "old_status">
+                    <select name="ticket_status" id="ticket_status" class="editTicketInputbox">
                             <?php
                                 foreach ($status as $singleStatus) {
                                     if($singleStatus["id"] === $ticket["status_id"]) continue;
@@ -89,41 +81,30 @@
                                 }
                             ?>
                     </select>    
-                    </p>
-                </div>
-                <button type="submit">Submit Change Status</button>
-            </form>
-    
-            <div class="changeTicket_inputBox">
-                <p>Change Agent Assigned To:</p>
-            </div>
-            <form action='../actions/change_ticket_assignment_action.php' method="post" class="editTicketForm">
-                <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
-                <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
-                <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-                <input type="hidden" value = "<?php echo $ticket['agent_id'] ?>" name = "old_agent">
-                <input type="hidden" value = "<?php echo getStatusName($ticket['status_id']) ?>" name = "old_status">
-                <div class="changeTicket_inputBox">
-                    <input type="text" name="newTicketAgent" placeholder ="Insert the name of the agent you want to assign this to or leave empty to remove any assignment"></input>
-                </div>
-                <div class="changeTicket_inputBox">
-                    <button type="submit">Submit Change Assignment</button>
-                </div>
-            </form>
-    
-            <div class="changeTicket_inputBox">
-                <p>Add Hashtag:</p>
-            </div>
-            <form action='../actions/add_hashtag_action.php' method="post" class="editTicketForm">
-                <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
-                <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-                <input class="changeTicket_inputBox" type="text" name="add_hashtag" id="add_hashtag" placeholder="#Add Hashtags" list="hashtag_list" autocomplete="on">
-                <datalist id="hashtag_list">
-                    <?php foreach ($hashtags as $hashtag){ ?>
-                        <option value="<?php echo $hashtag['name']; ?>"></option>
-                    <?php } ?>
-                </datalist>
-            </form>
+                    <button class = "changeTicketInfoButton" type="submit">Submit Change Status</button>
+                </form>
+        
+                <form action='../actions/change_ticket_assignment_action.php' method="post" class="editTicketForm">
+                    <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
+                    <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
+                    <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                    <input type="hidden" value = "<?php echo $ticket['agent_id'] ?>" name = "old_agent">
+                    <input type="hidden" value = "<?php echo getStatusName($ticket['status_id']) ?>" name = "old_status">
+                    <input type="text" name="newTicketAgent" placeholder ="Name of agent to be assigned"  class="editTicketInputbox"></input>
+                    <button class = "changeTicketInfoButton" type="submit">Submit Change Assignment</button>
+                </form>
+        
+                <form action='../actions/add_hashtag_action.php' method="post" class="editTicketForm">
+                    <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id"></input>
+                    <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                    <input class="editTicketInputbox" type="text" name="add_hashtag" id="add_hashtag" placeholder="#Add Hashtags" list="hashtag_list" autocomplete="on">
+                    <datalist id="hashtag_list">
+                        <?php foreach ($hashtags as $hashtag){ ?>
+                            <option value="<?php echo $hashtag['name']; ?>"></option>
+                        <?php } ?>
+                    </datalist>
+                </form>
+            </section>
     
         <?php } ?>
     
@@ -132,33 +113,33 @@
         <section id="singleTicket" class="activeTickets">
         </section>
     
-        <form method="post" id="insertAnswer" action="../actions/add_ticket_answer_action.php">
-            <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
-            <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id">
-            <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-            <textarea id="newAnswerText" name="newAnswerText" required></textarea>
-            <button type="submit">Post</button>
-        </form>
-        <?php if($_SESSION["type"] !== 'Client'){?>
-            <section class="faqAnswers">
-                <form method="post" id="insertFaqAnswer" action="../actions/add_ticket_answer_action.php">
-                    <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
-                    <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id">
-                    <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
-                    <select id="faqSelect" name="newAnswerText">
-                        <?php
-                            foreach($faq as $qa){
-                                echo '<option value="' . $qa["answer"] . '">' . $qa["question"] . '</option>';
-                            }
-                        ?>
-                    </select>
-                    <button type="submit">Quick Answer</button>
-                </form>
-            </section>
-        <?php } ?>   
-    
+        
         <section id="ticketAnswers" class ="activeTickets">
         </section>
+            <form method="post" id="insertAnswer" action="../actions/add_ticket_answer_action.php">
+                <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
+                <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id">
+                <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                <textarea id="newAnswerText" name="newAnswerText" required></textarea>
+                <button type="submit">Post</button>
+            </form>
+            <?php if($_SESSION["type"] !== 'Client'){?>
+                <section class="faqAnswers">
+                    <form method="post" id="insertFaqAnswer" action="../actions/add_ticket_answer_action.php">
+                        <input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>"></input>
+                        <input type="hidden" value = "<?php echo $ticketId ?>" name = "ticket_id">
+                        <input type="hidden" value = "<?php echo $_SESSION['id'] ?>" name = "user_id">
+                        <select id="faqSelect" name="newAnswerText">
+                            <?php
+                                foreach($faq as $qa){
+                                    echo '<option value="' . $qa["answer"] . '">' . $qa["question"] . '</option>';
+                                }
+                            ?>
+                        </select>
+                        <button type="submit">Quick Answer</button>
+                    </form>
+                </section>
+            <?php } ?>   
     </section>
 
     <script src="../scripts/ticket_page.js" defer></script>
